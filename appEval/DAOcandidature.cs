1a7088e664ab2f240a8a549a434c427b5bbb14af
johan <johanhamididu38@gmail.com>
2019-01-18 14:35:05 +0100

    Revert "Revert "debut form2 (offreEmlpoi et candidatures)""
    This reverts commit 67115f5382fd5b112d823e5bde17f07919ea340c.

---
+++ appEval/DAOcandidature.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace appEval
+{
+    public class DAOcandidature
+    {
+
+        public static List<candidature> AfficherCandidature()
+        {
+            var connString = "Host=localhost;Username=postgres;Password=;Database=appEval;port=5433";
+            List<candidature> lesCandidature = new List<candidature>();
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+
+
+                // Retrieve all rows
+                using (var cmd = new NpgsqlCommand("SELECT code FROM offre_emploi", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                    {
+                        candidature uneCandidature = new candidature(reader.GetString(0));
+                        lesCandidature.Add(uneCandidature);
+
+
+                    }
+
+            }
+            return lesCandidature;
+
+        }
+    }
+}
---
+++ appEval/candidature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appEval
+{
+    public class candidature
+    {
+        private string codeCandidat;
+
+
+        public candidature(string unCodeCandidat)
+        {
+            this.codeCandidat = unCodeCandidat;
+        }
+
+        public string GetCodeCandidat()
+        {
+            return this.codeCandidat;
+        }
+    }
+}
