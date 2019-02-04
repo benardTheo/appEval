126a19a4b5a18048177d8ffc1f6f83d5ca943f74
benard <benard.theo.sio@gmail.com>
2019-02-04 09:32:47 +0100

    debut dico

--- appEval/DAOresultatEval.cs
+++ appEval/DAOresultatEval.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace appEval
 {
     class DAOresultatEval
-    {
+    {        public static Dictionary<int, double> afficherEmplois()
+        {
+            var connString = "Host=localhost;Username=postgres;Password=;Database=appEval;";
+            Dictionary<int, double> moy = new Dictionary<int, double>();
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                // Retrieve all rows
+                using (var cmd = new NpgsqlCommand(
+                    "SELECT C.codecandidat, AVG(note) as 'Moyenne'"+
+                    "FROM candidature C"+
+                    "INNER JOIN evaluation ON C.codecandidat = evaluation.codecandidat"+
+                    "INNER JOIN noter ON evaluation.idevaluation = noter.idevaluation"+
+                    "GROUP BY C.codecandidat; ", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                    {
+                        
+                    }
+            }
+            return moy ;
+        }
     }
 }
--- appEval/listeEval.cs
+++ appEval/listeEval.cs
@@ -15,11 +15,34 @@ namespace appEval
         public listeEval()
         {
             InitializeComponent();
+            this.dataGridView1.Rows.Add(4);
+
+            this.dataGridView1[0,0].Value = "1";
+            this.dataGridView1[0, 1].Value = "2";
+            this.dataGridView1[0, 2].Value = "3";
+            this.dataGridView1[0, 3].Value = "4";
+            this.dataGridView1[0, 4].Value = "5";
+
+
+
         }
+
+        /*
+        SELECT c.codecandidat , note, nomprenom_rh
+        FROM candidature c
+        INNER JOIN evaluation e on e.codecandidat = c.codecandidat
+        INNER JOIN noter n on n.idevaluation = e.idevaluation
+        order by c.codecandidat;
+
+        SELECT C.codecandidat, AVG(note) as "Moyenne"
+        FROM candidature C
+        INNER JOIN evaluation ON C.codecandidat = evaluation.codecandidat
+        INNER JOIN noter ON evaluation.idevaluation = noter.idevaluation
+        GROUP BY C.codecandidat;
+        */
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
         }
     }
 }
