e029f5acb4865fcccc235ae0a4efc61ebaf7b689
johan <johanhamididu38@gmail.com>
2019-02-04 08:27:52 +0100

    list evalu

--- appEval/listeEval.Designer.cs
+++ appEval/listeEval.Designer.cs
@@ -28,19 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Classement = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Liste_candidats = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Note_moyenne = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(150, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Résultats des évaluations";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Classement,
+            this.Liste_candidats,
+            this.Note_moyenne});
+            this.dataGridView1.Location = new System.Drawing.Point(47, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 225);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Classement
+            // 
+            this.Classement.HeaderText = "Classement";
+            this.Classement.Name = "Classement";
+            // 
+            // Liste_candidats
+            // 
+            this.Liste_candidats.HeaderText = "Liste des candidats";
+            this.Liste_candidats.Name = "Liste_candidats";
+            // 
+            // Note_moyenne
+            // 
+            this.Note_moyenne.HeaderText = "Note moyenne";
+            this.Note_moyenne.Name = "Note_moyenne";
             // 
             // listeEval
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(585, 478);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
             this.Name = "listeEval";
             this.Text = "listeEval";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Classement;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Liste_candidats;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Note_moyenne;
     }
 }
--- appEval/listeEval.cs
+++ appEval/listeEval.cs
@@ -16,5 +16,10 @@ namespace appEval
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
